2d383fa42317ece79529eda529dc76dfff1f51fa
sangeedha <sangeedha20@gmail.com>
2024-01-25 17:52:49 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App appstyle = {{ borderColor: 'red', marginTop: '20px'}}
     name = 'input1'
     count = {5}/>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
